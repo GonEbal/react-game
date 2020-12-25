6bf28fc1bc0ee8653746b8d45a6085e0de50750e
GonEbal <66447247+GonEbal@users.noreply.github.com>
2020-12-25 13:40:04 -0800

    First Actions
    I created my first actions. Also created folders for components, middleware, reducers and utils

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React, { Component, Fragment } from 'react'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { connect } from 'react-redux'
+import LoadingBar from 'react-redux-loading'
+
+
+function App() {
+  return (
+    <div>
+      Hi
+    </div>
+  )
+}
+
+export default App
