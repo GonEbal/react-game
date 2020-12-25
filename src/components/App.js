1890b392245ab2166cfef514b0413b8f8cb6e60d
GonEbal <66447247+GonEbal@users.noreply.github.com>
2020-12-25 14:38:00 -0800

    Loading Bar

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,33 @@
-import React, { Component, Fragment } from 'react'
-import { BrowserRouter as Router, Route } from 'react-router-dom'
-import { connect } from 'react-redux'
-import LoadingBar from 'react-redux-loading'
-import { handleInitialData } from '../actions/shared'
-
+import React, { Component, Fragment } from "react"
+import { BrowserRouter as Router, Route } from "react-router-dom"
+import { connect } from "react-redux"
+import LoadingBar from "react-redux-loading"
+import { handleInitialData } from "../actions/shared"
 
 class App extends Component {
-    componentDidMount() {
-        this.props.dispatch(handleInitialData())
-    }
-    render() {
-        return (
-            <div>
-        Hi
-      </div>
-        )
-    }
+	componentDidMount() {
+		this.props.dispatch(handleInitialData())
+	}
+	render() {
+		return (
+			<Router>
+				<Fragment>
+					<LoadingBar />
+					<div className="container">
+						{this.props.loading === true 
+							? null 
+							: <div>Hello</div>}
+					</div>
+				</Fragment>
+			</Router>
+		)
+	}
 }
 
-export default connect()(App)
+function mapStateToProps ({ authedUser }) {
+  return {
+    loading: authedUser === null
+  }
+}
+
+export default connect(mapStateToProps)(App)
