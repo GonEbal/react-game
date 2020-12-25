81e96ebdf3c346f0ea5cf44590c121fdce2b143e
GonEbal <66447247+GonEbal@users.noreply.github.com>
2020-12-25 13:51:45 -0800

    First Reducers
    Reducers were created.

--- src/index.js
+++ src/index.js
@@ -4,10 +4,16 @@ import './index.css'
 import App from './components/App'
 import { createStore, compose } from 'redux'
 import { Provider } from 'react-redux'
+import reducer from './reducers'
+
+const store = createStore(
+	reducer, 
+	window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+	)
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-)
+  </Provider>,
+document.getElementById('root')
+) 
