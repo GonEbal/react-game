067720e5649699d877b99e4d7206dc6d7a1d551a
GonEbal <66447247+GonEbal@users.noreply.github.com>
2020-12-28 23:37:15 -0800

    CSS changes

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -3,6 +3,10 @@ import { connect } from "react-redux"
 import Question from "./Question"
 
 class Dashboard extends Component {
+  state ={
+    show_answered: true,
+  }
+
   unanswered = () => {
     const container = document.getElementById("container_list")
     container.classList.add("right-panel-active")
@@ -11,50 +15,32 @@ class Dashboard extends Component {
     const container = document.getElementById("container_list")
     container.classList.remove("right-panel-active")
   }
+
+  change = () => {
+    this.setState({ show_answered: !this.state.show_answered })
+  }
+
   render() {
-    console.log(this.props.questionIds)
-    console.log(this.props.answered_q)
     return (
       <div className="container_body">
-        <div className="container_list" id="container_list">
-          <div className="form-container_list unanswered-container_list questions_list">
-            <h1>Unanswered</h1>
-            <ul className="dashboard-list">
-              {this.props.unanswered_q.map((id) => (
-                <li key={id} className="question">
-                  <Question id={id} />
-                </li>
-              ))}
-            </ul>
-          </div>
-          <div className="form-container_list answered-container_list questions_list">
-            <h1>Answered</h1>
-            <ul className="dashboard-list">
-              {this.props.answered_q.map((id) => (
-                <li key={id} className="question">
-                  <Question id={id} />
-                </li>
-              ))}
-            </ul>
-          </div>
-          <div className="overlay-container_list">
-            <div className="overlay">
-              <div className="overlay-panel overlay-left">
-                <h1>Don't feel to answer?</h1>
-                <p>Wanna see answered questions?</p>
-                <button className="ghost" id="signIn" onClick={this.answered}>
-                  View Answered
-                </button>
-              </div>
-              <div className="overlay-panel overlay-right">
-                <h1>Want more questions?</h1>
-                <p>Check out all questions</p>
-                <button className="ghost" id="signUp" onClick={this.unanswered}>
-                  View
-                </button>
-              </div>
-            </div>
-          </div>
+      <button
+      onClick={this.change}
+      >
+        Change
+      </button>
+        <div className='check' style={{display: this.state.show_answered ? 'block' : 'none' }}>
+          {this.props.answered_q.map((id) => (
+            <li key={id} className="question">
+              <Question id={id} />
+            </li>
+          ))}
+        </div>
+         <div style={{display: !this.state.show_answered ? 'block' : 'none' }}>
+          {this.props.unanswered_q.map((id) => (
+            <li key={id} className="question">
+              <Question id={id} />
+            </li>
+          ))}
         </div>
       </div>
     )
@@ -63,19 +49,19 @@ class Dashboard extends Component {
 
 function mapStateToProps({ questions, authedUser, users }) {
   const answered_questions = Object.values(questions).filter(
-    valueName => (valueName.optionOne.votes).includes(authedUser) 
-    || (valueName.optionTwo.votes).includes(authedUser))
+    (valueName) =>
+      valueName.optionOne.votes.includes(authedUser) ||
+      valueName.optionTwo.votes.includes(authedUser)
+  )
 
   const unanswered_questions = Object.values(questions).filter(
-    valueName => (!(valueName.optionOne.votes).includes(authedUser)) 
-    && (!(valueName.optionTwo.votes).includes(authedUser)))
+    (valueName) =>
+      !valueName.optionOne.votes.includes(authedUser) &&
+      !valueName.optionTwo.votes.includes(authedUser)
+  )
   return {
-    unanswered_q: Object.values(unanswered_questions).map(
-    item => item.id
-    ),
-    answered_q: Object.values(answered_questions).map(
-    item => item.id
-    )
+    unanswered_q: Object.values(unanswered_questions).map((item) => item.id),
+    answered_q: Object.values(answered_questions).map((item) => item.id),
   }
 }
 
--- src/components/Question.js
+++ src/components/Question.js
@@ -1,23 +1,35 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux'
+import React, { Component, Fragment } from "react";
+import { connect } from "react-redux";
 
 class Question extends Component {
 	render() {
+		const { avatarURL, name, id } = this.props.user;
+		const { optionOne, optionTwo } = this.props.question
 		return (
-			<div>
-				{this.props.id}
-				{this.props.authedUser}
-			</div>
+			<Fragment>
+				<img
+					src={avatarURL}
+					alt={`Avatar of ${name}`}
+					className="avatar"
+				/>
+				<div className="tweet-info">
+					<div>
+						<span>{name}</span>
+						<p>{optionOne.text}</p>
+					</div>
+				</div>
+			</Fragment>
 		);
 	}
 }
 
-function mapStateToProps ({authedUser, users}) {
-
-  return {
-  	authedUser,
-  	users
-  }
+function mapStateToProps({ questions, users }, { id }) {
+	const question = questions[id];
+	const user = users[question.author];
+	return {
+		question: question,
+		user: user,
+	};
 }
 
-export default connect(mapStateToProps)(Question)
+export default connect(mapStateToProps)(Question);
