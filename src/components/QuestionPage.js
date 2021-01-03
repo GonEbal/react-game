b4d41def5339d03a47c93ecd021104a464c83b4f
GonEbal <66447247+GonEbal@users.noreply.github.com>
2021-01-02 19:30:06 -0800

    QuestionPage  JSX+CSS

--- src/components/QuestionPage.js
+++ src/components/QuestionPage.js
@@ -1,24 +1,68 @@
-import React, { Component } from 'react';
+import React, { Component } from "react"
 import { connect } from "react-redux"
 
 class QuestionPage extends Component {
 	render() {
-		const { id } = this.props
+		const { question } = this.props
+		const { avatarURL, name } = this.props.author
 		return (
-			<div>
-				{id}
+			<div className="container_body">
+				<div className="user_asks">
+					<span>{name} asks:</span>
+				</div>
+				<div className="question-inner">
+					<img
+						src={avatarURL}
+						alt={`Avatar of ${name}`}
+						className="avatar"
+					/>
+					<div className="question-info">
+						<div>
+							<span>Would you rather...</span>
+						</div>
+						<div>
+							<form className="radio_input">
+								<input
+									type="radio"
+									id="optionOne"
+									name="option"
+									value="optionOne"
+								/>
+								<label for="optionOne">
+									{question.optionOne.text}
+								</label>
+								<br />
+								<input
+									type="radio"
+									id="optionTwo"
+									name="option"
+									value="optionTwo"
+								/>
+								<label for="optionTwo">
+									{question.optionTwo.text}
+								</label>
+								<button className="submit_poll_btn">
+									Submit
+								</button>
+							</form>
+						</div>
+					</div>
+				</div>
 			</div>
-		);
+		)
 	}
 }
 
-function mapStateToProsp({ authedUser }, props) {
+function mapStateToProsp({ authedUser, questions, users }, props) {
 	const { id } = props.match.params
+	const question = questions[id]
+	const author = users[question.author]
 
 	return {
 		id,
+		author,
+		question: question,
 	}
 }
 
 export default connect(mapStateToProsp)(QuestionPage)
-
