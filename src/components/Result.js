a8d7781269470e9ebbd7b8c14684fac92b9a7859
GonEbal <66447247+GonEbal@users.noreply.github.com>
2021-01-04 19:24:30 -0800

    Result Component CSS

--- src/components/Result.js
+++ src/components/Result.js
@@ -1,15 +1,15 @@
-import React, { Component } from "react"
-import { connect } from "react-redux"
+import React, { Component } from "react";
+import { connect } from "react-redux";
 
 class Result extends Component {
 	render() {
-		const { avatarURL, name } = this.props.author
+		const { avatarURL, name } = this.props.author;
 		const {
 			total_answers,
 			authedUser_answer,
 			this_answer,
 			progress,
-		} = this.props
+		} = this.props;
 		return (
 			<div className="container_body">
 				<div className="user_asks">
@@ -25,43 +25,51 @@ class Result extends Component {
 						<div>
 							<span>Results:</span>
 						</div>
-						<div>
+						<div className='option one'>
 							<p>Would you ...</p>
-							<p>{progress}%</p>
-							<p>
-								{this_answer} of {total_answers} votes
+							<div className="progress_bar"> 
+						        <div style={{width: `${progress}%`}} className="progress">{progress}%</div> 
+						    </div> 
+							<p className='answer_count'>
+								{this_answer} out of {total_answers} votes
 							</p>
-							<progress id="file" value={progress} max="100">
-								{" "}
-								{progress}%{" "}
-							</progress>
+						</div>
+						<div className='option two'>
+							<p>Would you ...</p>
+							<div className="progress_bar"> 
+						        <div style={{width: `${progress}%`}} className="progress">{progress}%</div> 
+						    </div> 
+							<p className='answer_count'>
+								{this_answer} out of {total_answers} votes
+							</p>
 						</div>
 					</div>
 				</div>
 			</div>
-		)
+		);
 	}
 }
 
 function mapStateToProsp({ authedUser, questions, users }, props) {
-	const { id } = props.match.params
-	const question = questions[id]
-	const author = users[question.author]
+	const { id } = props.match.params;
+	const question = questions[id];
+	const author = users[question.author];
 	const total_answers =
 		Object.keys(question.optionTwo.votes).length +
-		Object.keys(question.optionOne.votes).length
+		Object.keys(question.optionOne.votes).length;
 
-	const authedUser_answer = users[authedUser].answers[id]
+	const authedUser_answer = users[authedUser].answers[id];
 
-	const this_answer = Object.keys(question[authedUser_answer].votes).length
+	const this_answer = Object.keys(question[authedUser_answer].votes).length;
 
 	return {
+
 		total_answers,
 		authedUser_answer,
 		this_answer,
 		author,
 		progress: ((this_answer * 100) / total_answers).toFixed(1),
-	}
+	};
 }
 
-export default connect(mapStateToProsp)(Result)
+export default connect(mapStateToProsp)(Result);
