000878857d00c70f3467185af2e563eeb156953d
GonEbal <66447247+GonEbal@users.noreply.github.com>
2021-01-05 16:49:47 -0800

    NewQuestion State

--- src/components/NewQuestion.js
+++ src/components/NewQuestion.js
@@ -1,6 +1,18 @@
 import React, { Component } from "react"
 
 class NewQuestion extends Component {
+	state = {
+		optionOne: "",
+		optionTwo: "",
+	}
+
+	handleChange = (e) => {
+		const value = e.target.value
+		this.setState((previousState) => ({
+			...previousState,
+			[e.target.name]: value,
+		}))
+	}
 	render() {
 		return (
 			<div className="container_body">
@@ -8,19 +20,31 @@ class NewQuestion extends Component {
 					<h1>Create New Question</h1>
 				</div>
 				<div className="newq-inner">
-					<div className='newq_text'>
+					<div className="newq_text">
 						<span>Complete the question:</span>
 					</div>
-					<div className='newq_text'>
+					<div className="newq_text">
 						<span>Would you rather ...</span>
 					</div>
 					<div className="newq-info">
 						<form className="create_form">
-							<input type="text" placeholder='Enter Option One Text Here' />
+							<input
+								type="text"
+								name="optionOne"
+								value={this.state.optionOne}
+								onChange={this.handleChange}
+								placeholder="Enter Option One Text Here"
+							/>
 							<br />
 							<span>OR</span>
 							<br />
-							<input type="text" placeholder='Enter Option Two Text Here' />
+							<input
+								type="text"
+								name="optionTwo"
+								value={this.state.optionTwo}
+								onChange={this.handleChange}
+								placeholder="Enter Option Two Text Here"
+							/>
 							<button className="submit_new_btn" type="submit">
 								Submit
 							</button>
